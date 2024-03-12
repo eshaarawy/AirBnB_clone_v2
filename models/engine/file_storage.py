4070c333a99b1dd59aa7b4ec6adb21b5dcbac8b4
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-03-12 21:45:17 +0200

    First 5 tasks

---
+++ models/engine/file_storage.py
@@ -0,0 +1,30 @@
+import json
+import os
+from models.base_model import BaseModel
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return

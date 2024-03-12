998ae896081af6e90d9efcdd0604209ae1d21384
mariambingoo <mariamsheta13@gmail.com>
2024-03-12 23:50:03 +0200

    tets folder created

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/base_model.py."""
+"""Defines tests for models/base_model.py."""
 from models.base_model import BaseModel
 
 my_model = BaseModel()
